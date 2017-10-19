05758bc0f9e0770f898b5f71025684b9d2f76564
orion55 <grol55@mail.ru>
2017-10-19 22:16:01 +0500

    slick two

--- docs/js/main.js
+++ docs/js/main.js
@@ -8,4 +8,14 @@ $(document).ready(function () {
         prevArrow: $('.slider__arrow-left'),
         nextArrow: $('.slider__arrow-right')
     });
+    $('.slid').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        prevArrow: $('.slid__arrow-left'),
+        nextArrow: $('.slid__arrow-right')
+    });
 });
